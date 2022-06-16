734e5830d1cad4e47fa8716c9fa872835178503b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-16 17:46:10 -0400

    Created bricks props

--- script.js
+++ script.js
@@ -26,6 +26,16 @@ const paddle = {
     visible: true
 };
 
+// Create brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+};
+
 
 
 // Draw ball on canvas
