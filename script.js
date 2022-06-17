7834e1ca7859abbeda8570fd30576c16dccf4a20
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-17 00:01:27 -0400

    Implemented keydown and key up functionality

--- script.js
+++ script.js
@@ -108,6 +108,27 @@ function update() {
 
 update();
 
+// Keydown event
+function keyDown(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        paddle.dx = paddle.speed;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = -paddle.speed;
+  }
+}
+
+// Keyup event
+function keyUp(e) {
+    if (
+        e.key === 'Right' ||
+        e.key === 'ArrowRight' ||
+        e.key === 'Left' ||
+        e.key === 'ArrowLeft'
+    ) {
+        paddle.dx = 0;
+    }
+}
+
 // Keyboard event handlers
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
